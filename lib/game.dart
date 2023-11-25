13fea1c22b760c1b7890eec28bbb8a3b8810299a
Christopher Zenzel <christopher@arendellekingdom.com>
2023-11-25 15:46:38 -0800

    Enhancements to score keeping

--- lib/game.dart
+++ lib/game.dart
@@ -3,11 +3,14 @@ import 'scorer.dart';
 class Game {
   var _currentFrame = 0;
   var _isFirstThrow = true;
+  var _isFirstInFrame = true;
   var scorer = Scorer();
 
   int get score => scoreForFrame(_currentFrame);
+  int _lastPins = 0;
 
   void add(int pins) {
+    _lastPins = pins;
     scorer.addThrow(pins);
     _adjustCurrentFrame(pins);
   }
@@ -15,20 +18,21 @@ class Game {
   void calculatedAdd(String pins) {
     if (pins == "X") {
       add(10);
-    } else if (score > 0 && pins == "/") {
-      add(10 - score);
+    } else if (_lastPins >= 0 && pins == "/") {
+      add(10 - _lastPins);
     }
   }
 
   void _adjustCurrentFrame(int pins) {
     if (_lastBallInFrame(pins)) {
+      _isFirstInFrame = true;
+      _advanceFrame();
+    } else if (!_isFirstInFrame) {
+      _isFirstInFrame = true;
       _advanceFrame();
     } else {
+      _isFirstInFrame = false;
       _isFirstThrow = false;
-
-      if (!_isFirstThrow && _currentFrame < 10) {
-        _advanceFrame();
-      }
     }
   }
 
